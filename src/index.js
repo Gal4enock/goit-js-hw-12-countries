3dae481682b6839b3d59fdb53da6aa277fa80793
Galyna Khyzhynska <gal4enock86@gmail.com>
2020-10-30 08:42:39 +0200

    добавляет стили

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import './styles.css';
 import debounce from 'lodash.debounce';
 import handlebars from 'handlebars';
-import toPars from './templates/pars.hbs'
-console.log(toPars);
+import pars from './templates/pars.hbs';
+
+import { alert, notice, info, success, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+
+
+
 
 const refs = {
   box: document.querySelector('.qwery'),
   input: document.querySelector('[name="country"]'),
   list: document.querySelector('.list')
 }
-console.dir(refs.input);
 
 let word = '';
 refs.input.addEventListener('input', debounce((e) => {
-  word = e.target.value
-  console.log(word);
-  console.dir(refs.input);
+  word = e.target.value;
   refs.list.innerHTML = '';
   getACountry()
 }, 500))
@@ -31,15 +33,13 @@ const getACountry = function () {
       if (arr.length > 10) {
         alert('Too many mathes found. Please enter more specific query!')
       } 
-      // console.log(arr);
       else if (arr.length > 1 && arr.length <= 10) {
         console.log(arr);
         arr.map((el) => refs.list.insertAdjacentHTML('beforeend', `<li> ${el.name}</li>`))
       }
       else if (arr.length === 1) {
         const obj = arr[0];
-        const tag = toPars(obj);
-        refs.list.insertAdjacentHTML('beforeend', tag)
+        refs.list.innerHTML = pars(obj)
       }
     })
 }
