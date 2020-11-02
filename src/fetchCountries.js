cdaf262ae1389800cdddc1690062ace82f185f59
Galyna Khyzhynska <gal4enock86@gmail.com>
2020-11-02 14:34:02 +0200

    исправляет модуль

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,3 @@
-import handlebars from 'handlebars';
-import pars from './templates/pars.hbs';
 import { alert, notice, info, success, error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 
@@ -9,23 +7,16 @@ const refs = {
   list: document.querySelector('.list')
 }
 
-export default function fetchCountries (url) {
-  fetch(url)
-    .then(res => {
-      if (res.status >= 200 && res.status < 300) {
-        return res.json()
-      } else alert('Вы ввели неверное название!')
-    })
-    .then(arr => {
-      if (arr.length > 10) {
-        info('Too many mathes found. Please enter more specific query!')
-      } 
-      else if (arr.length > 1 && arr.length <= 10) {
-        arr.map((el) => refs.list.insertAdjacentHTML('beforeend', `<li> ${el.name}</li>`))
-      }
-      else if (arr.length === 1) {
-        const obj = arr[0];
-        refs.list.innerHTML = pars(obj)
-      }
-    })
+
+export function fetchCountries (word) {
+  let url = `https://restcountries.eu/rest/v2/name/${word}`;
+  return fetch(url)
+  .then(res => {
+    
+        if (res.status >= 200 && res.status < 300) {
+          return res.json()
+        }
+        else alert('Вы ввели неверное название!')
+      })
 }
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './styles.css';
-import fetchCountries from './fetchCountries.js';
+import {fetchCountries} from './fetchCountries';
 import debounce from 'lodash.debounce';
 import handlebars from 'handlebars';
+import pars from './templates/pars.hbs';
+import { alert, notice, info, success, error } from '@pnotify/core';
 
 const refs = {
   box: document.querySelector('.qwery'),
@@ -10,14 +12,31 @@ const refs = {
 }
 
 let word = '';
+let url = '';
 refs.input.addEventListener('input', debounce((e) => {
   word = e.target.value;
-   if (word === '') {
+  if (word === '') {
     return
   }
-  let url = `https://restcountries.eu/rest/v2/name/${word}`;
   refs.list.innerHTML = '';
-  fetchCountries(url)
+  url = `https://restcountries.eu/rest/v2/name/${word}`;
+  render()
 }, 500))
 
-
+// let url = `https://restcountries.eu/rest/v2/name/${word}`;
+// fetchCountries('usa').then(data => console.log(data))
+const render = function () {
+  fetchCountries(word)
+    .then(arr => {
+      if (arr.length > 10) {
+        info('Too many mathes found. Please enter more specific query!')
+      } 
+      else if (arr.length > 1 && arr.length <= 10) {
+        arr.map((el) => refs.list.insertAdjacentHTML('beforeend', `<li> ${el.name}</li>`))
+      }
+      else if (arr.length === 1) {
+        const obj = arr[0];
+        refs.list.innerHTML = pars(obj)
+      }
+    })
+}
